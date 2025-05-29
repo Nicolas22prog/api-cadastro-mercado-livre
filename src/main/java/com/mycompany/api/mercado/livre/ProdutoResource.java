215498324fb9e570a3739251549ab01bbe131303
Nicolas <nicolasmoneteiro8@gmail.com>
2025-05-29 13:30:59 -0300

    adiciona filtro para nao importar os produtos que ja estao cadastrados no banco de dados

--- src/main/java/com/mycompany/api/mercado/livre/ProdutoResource.java
+++ src/main/java/com/mycompany/api/mercado/livre/ProdutoResource.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.api.mercado.livre;
 
+
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -16,9 +17,12 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/produtos")
 @Produces(MediaType.APPLICATION_JSON)
@@ -41,12 +45,38 @@ public class ProdutoResource {
     @Path("/importar")
     @Transactional
     public Response importarVarios(@QueryParam("ids")String idsParam) {
-       
-        List<Produto> produtos = mls.importarProduto(idsParam);
-        for (Produto p : produtos) {
+        //Converte a string de entrada para uma lista de IDs(strings)
+    List<String> idsRecebidos = Arrays.stream(idsParam.split(","))
+            .map(String::trim)
+            .collect(Collectors.toList());
+    
+    //Buscar por IDs que já existam no banco
+    
+    List<String> idsExistentes = em.createQuery(
+            "SELECT p.idExterno FROM Produto p WHERE p.idExterno IN :ids", String.class)
+            .setParameter("ids", idsRecebidos)
+            .getResultList();
+    
+    //Filtrar os que ainda nao existem no banco
+    
+    List<String> idsParaImportar = idsRecebidos.stream()
+            .filter(id-> !idsExistentes.contains(id))
+            .collect(Collectors.toList());
+    
+    
+    //IMporta os produtos restantes
+    
+    List<Produto> produtosImportados= new ArrayList<>();
+    for (String id : idsParaImportar) {
+        List<Produto> produtos = mls.importarProduto(id);
+        for(Produto p: produtos) {
             em.persist(p);
+            produtosImportados.add(p);
         }
-        return Response.status(Response.Status.CREATED).entity(produtos).build();
     }
-
+    
+    return Response.status(Response.Status.CREATED)
+            .entity(produtosImportados)
+            .build();
+}
 }
